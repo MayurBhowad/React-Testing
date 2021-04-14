7a4ab656efa8e1b36b4627306d6592fd03a34cf6
MayurBhowad <mayurbhowad1995@gmail.com>
2021-04-14 14:18:10 +0530

    add: Navbar test case

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react'
 export class Navbar extends Component {
     render() {
         return (
-            <div>
-
+            <div ClassName='navbar'>
+                <div className="logo">LOGO</div>
+                <ul className="nav-list">
+                    <li className="list-item"><a href="#" className="item">Home</a></li>
+                    <li className="list-item"><a href="#" className="item">About</a></li>
+                    <li className="list-item"><a href="#" className="item">Contact</a></li>
+                    <li className="list-item"><a href="#" className="item">Login</a></li>
+                </ul>
             </div>
         )
     }
---
+++ src/components/navbar/__test__/Navbar.test.js
@@ -0,0 +1,20 @@
+import { shallow } from 'enzyme';
+import Navbar from '../Navbar';
+
+describe('Navbar testing', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<Navbar />);
+    })
+
+    test('Confirm logo', () => {
+        expect(wrapper.find('.logo').text()).toEqual('LOGO')
+    })
+
+    test('confirm nav list item', () => {
+        let items = ["Home", "About", "Contact", "Login"];
+        wrapper.find(".list-item").forEach((item, i) => {
+            expect(item.text()).toEqual(items[i]);
+        })
+    })
+})
