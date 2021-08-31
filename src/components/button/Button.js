2c71a98f3d4f62b18ec6a571e079f592eac518d7
MAYUR_M_BHOWAD <32778712+MayurBhowad@users.noreply.github.com>
2021-08-31 19:07:21 +0530

    Update Button.js

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -3,7 +3,7 @@ import './button.css';
 
 function Button({ lable }) {
 
-    return <div data-testid="button" className="button-style" >Hi {lable}</div>
+    return <div data-testid="button" className="button-style" >Hello {lable}</div>
 }
 
 export default Button
